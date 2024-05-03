3fa5268d3f029e60cefeabf52bb5ec45ada04585
Pyrocob <pyrocob@gmail.com>
2024-05-03 18:07:08 +0200

    [fix] skull pickable

---
+++ Assets/Scripts/B_Script/B_SkullPïckable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B_SkullPïckable : Interactive
+{
+    public KeyItemData data;
+    public bool fromFloor = true;
+}
+
